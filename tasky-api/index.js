1317bac76d06e1df6b8da4e7ca53405006fc3088
rbirney <rbirney@wit.ie>
2026-03-25 08:03:48 +0000

    Mongo lab Error Handling

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -5,6 +5,15 @@ import './db';
 
 dotenv.config();
 
+const errHandler = (err, req, res, next) => {
+  /* if the error in development then send stack trace to display whole error,
+  if it's in production then just send error message  */
+  if(process.env.NODE_ENV === 'production') {
+    return res.status(500).send(`Something went wrong!`);
+  }
+  res.status(500).send(`Hey!! You caught the error 👍👍. Here's the details: ${err.stack} `);
+};
+
 const app = express();
 
 const port = process.env.PORT;
@@ -14,6 +23,8 @@ app.use(express.static('public'));
 app.use(express.json());
 
 app.use('/api/tasks', tasksRouter);
+
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
